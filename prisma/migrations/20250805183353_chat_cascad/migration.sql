d91164bb4b11896108e18ec71cf77c60b66675c6
Andas-LV <andassatybaldy@gmail.com>
2025-08-09 12:21:18 +0500

    feat: regenerate migrates

--- prisma/migrations/20250805183353_chat_cascad/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Request" DROP CONSTRAINT "Request_chatId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Request" ADD CONSTRAINT "Request_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
