339c61f1a86d0a0bd2c56f246f1abd5422f04e86
Andas-LV <andassatybaldy@gmail.com>
2025-08-09 12:21:18 +0500

    feat: regenerate migrates

--- prisma/migrations/20250809071912_regenerate/migration.sql
+++ prisma/migrations/20250809071912_regenerate/migration.sql
@@ -1,13 +1,25 @@
-/*
-  Warnings:
+-- CreateEnum
+CREATE TYPE "Provider" AS ENUM ('gemini', 'deepseek', 'chatgpt', 'claude', 'llama');
 
-  - A unique constraint covering the columns `[id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "providerId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-*/
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "Chat" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "aiProvider" "Provider" NOT NULL,
     "authorId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -29,8 +41,11 @@ CREATE TABLE "Request" (
 -- CreateIndex
 CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_provider_providerId_key" ON "User"("provider", "providerId");
+
 -- AddForeignKey
 ALTER TABLE "Chat" ADD CONSTRAINT "Chat_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Request" ADD CONSTRAINT "Request_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Request" ADD CONSTRAINT "Request_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE CASCADE ON UPDATE CASCADE;
