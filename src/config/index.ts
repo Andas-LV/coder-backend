cea6dbb5af2dbb07fa9ae821643427b719b9c73a
Andas-LV <andassatybaldy@gmail.com>
2025-08-12 13:32:49 +0500

    CLIENT_URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 interface Config {
 	port: number;
 	nodeEnv: string;
-	origins: string;
+	origins: string[];
 	localhost: string;
 	nextAuthSecret: string;
 	deepSeek_api: string;
@@ -17,12 +17,19 @@ interface Config {
 const config: Config = {
 	port: Number(process.env.PORT) || 8000,
 	nodeEnv: process.env.NODE_ENV || 'development',
-	origins: process.env.CLIENT_URL || '*',
+	origins: [
+		process.env.CLIENT_URL || 'https://coder-front.vercel.app',
+		'http://localhost:3000',
+	],
 	localhost: process.env.LOCALHOST || 'http://127.0.0.1:8000',
-	nextAuthSecret: process.env.NEXTAUTH_SECRET || 'O/7OG8x3dlmW4dEU1yT/OBz5yKfq0KN2hmNHUPH8XSM=',
-	deepSeek_api: process.env.DEEPSEEK_BASE_URL || "https://api.deepseek.com",
-	deepSeek_api_key: process.env.DEEPSEEK_API_KEY || "sk-5152973e795b46f48eabcb99beabad58",
-	gemini_api_key: process.env.GEMIN_API_KEY || 'AIzaSyBUlchyiOCRoX7gh_jyDuUHGaI2e2HWb1E',
+	nextAuthSecret:
+		process.env.NEXTAUTH_SECRET ||
+		'O/7OG8x3dlmW4dEU1yT/OBz5yKfq0KN2hmNHUPH8XSM=',
+	deepSeek_api: process.env.DEEPSEEK_BASE_URL || 'https://api.deepseek.com',
+	deepSeek_api_key:
+		process.env.DEEPSEEK_API_KEY || 'sk-5152973e795b46f48eabcb99beabad58',
+	gemini_api_key:
+		process.env.GEMIN_API_KEY || 'AIzaSyBUlchyiOCRoX7gh_jyDuUHGaI2e2HWb1E',
 };
 
 export default config;
