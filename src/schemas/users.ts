b1c8cc1d975e9bb87c9221b3ec6756ff815f357d
Andas-LV <andassatybaldy@gmail.com>
2025-08-20 13:33:43 +0500

    feat: added user auth providers

--- src/schemas/users.ts
+++ src/schemas/users.ts
@@ -1,10 +1,11 @@
 import { z } from 'zod';
+import { AuthProvider } from '@prisma/client';
 
 export const userSchema = z.object({
     id: z.string(),
     email: z.string().min(1),
     name: z.string().nullable(),
     image: z.string().nullable(),
-    provider: z.string(),
+    provider: z.nativeEnum(AuthProvider),
     providerId: z.string(),
 });
